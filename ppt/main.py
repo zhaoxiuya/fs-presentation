5e74b1e5a08ed8467980bff0f388ded412134bbf
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-31 04:35:32 +0900

    help

--- ppt/main.py
+++ ppt/main.py
@@ -1,5 +1,6 @@
 from manim import *
 from manim_slides import *
+import numpy as np
 
 class MyPpt(Slide):
     BZ = 1.5
@@ -7,9 +8,10 @@ class MyPpt(Slide):
     SZ = 0.5
 
     def construct(self):
-        self.show_intro()
-        self.show_index()
-        self.show_gopsem()
+        # self.show_intro()
+        # self.show_index()
+        # self.show_gopsem()
+        self.show_multiply()
 
     def show_intro(self):
         title = Text("제임스와 존의 곱셈의 비밀").scale(self.BZ)
@@ -51,4 +53,60 @@ class MyPpt(Slide):
             self.play(FadeIn(i, shift=RIGHT*0.5))
             self.next_slide()
         self.play(FadeOut(imgs))
+        self.next_slide()
+
+    def show_multiply(self):
+        a_str, b_str = "260131", "091227"
+
+        a = VGroup(*[Text(i) for i in a_str]).arrange(RIGHT, buff=0.2)
+        b = VGroup(*[Text(i) for i in b_str]).arrange(RIGHT, buff=0.2)
+        b.next_to(a, DOWN, aligned_edge=RIGHT)
+
+        x = MathTex(r"\times").next_to(b, LEFT).scale(self.MZ)
+        line = Line(LEFT, RIGHT).set_width(a.width + 0.8).next_to(b, DOWN, buff=0.1)
+
+        setup = VGroup(a, b, x, line).to_edge(UP, buff=0.5)
+        self.play(Write(setup))
+        self.next_slide()
+
+        l = [VGroup() for i in range(len(b_str))]
+        for i in range(len(b_str))[::-1]:
+            tmp = str(int(b_str[i])*int(a_str))
+            if len(tmp) < len(b_str):
+                tmp = "0"*(len(b_str)-len(tmp)) + tmp
+            l[i] = VGroup(*[Text(x) for x in tmp]).arrange(RIGHT, buff=0.2)
+        l += [b]
+
+        for i in range(len(b_str))[::-1]:
+            l[i].next_to(l[i+1], DOWN, aligned_edge=RIGHT)
+            self.play(b[i].animate.set_color(RED))
+            if i!=len(b_str)-1:
+                l[i].shift(LEFT * 0.5)
+            for j in range(len(b_str))[::-1]:
+                self.play(a[j].animate.set_color(BLUE),run_time=0.15)
+                if(len(b_str)<len(l[i])):
+                    if j == 0:
+                        self.play(l[i][j+1].animate.set_color(GOLD),run_time=0.15)
+                        self.play(FadeIn(l[i][j+1]),run_time=0.15)
+                        self.play(l[i][j+1].animate.set_color(WHITE),run_time=0.15)
+                        self.play(l[i][j].animate.set_color(GOLD),run_time=0.15)
+                        self.play(FadeIn(l[i][j]),run_time=0.15)
+                        self.play(l[i][j].animate.set_color(WHITE),run_time=0.15)
+                    else:
+                        self.play(l[i][j+1].animate.set_color(GOLD),run_time=0.15)
+                        self.play(FadeIn(l[i][j+1]),run_time=0.15)
+                        self.play(l[i][j+1].animate.set_color(WHITE),run_time=0.15)
+                else:
+                    self.play(l[i][j].animate.set_color(GOLD),run_time=0.15)
+                    self.play(FadeIn(l[i][j]), run_time=0.15)
+                    self.play(l[i][j].animate.set_color(WHITE),run_time=0.15)
+                self.play(a[j].animate.set_color(WHITE),run_time=0.15)
+            self.play(b[i].animate.set_color(WHITE),run_time=0.15)
+
+
+        line2 = (Line(LEFT, RIGHT).set_width(2 * a.width + 0.8).next_to(l[0], DOWN, buff=0.1, aligned_edge=RIGHT).align_to(b, RIGHT))
+        plus = MathTex(r"+").next_to(b, LEFT).scale(self.MZ)
+
+        setup2 = VGroup(plus, line2).to_edge(UP, buff=0.5)
+        self.play(Write(setup2))
         self.next_slide()
