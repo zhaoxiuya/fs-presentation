05c3a95bffd705cd8ce6ca2861c1f8904e051346
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-30 10:43:47 +0900

    flake

--- flake.nix
+++ flake.nix
@@ -17,11 +17,18 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        hpkgs = pkgs.haskell.packages.ghc912;
+        py = pkgs.python313Packages;
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             gfortran15
+            hpkgs.ghc
+            py.numpy
+            py.scipy
+            py.pandas
+            py.matplotlib
           ];
         };
       }
