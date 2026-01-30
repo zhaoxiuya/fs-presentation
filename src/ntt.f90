4f359637bf873f19fc1381d5f2fd2eadb4d3a1b0
zhaoxiuya <zhaoxiuya13@gmail.com>
2026-01-30 10:18:45 +0900

    help

--- src/ntt.f90
+++ src/ntt.f90
@@ -1,51 +1,57 @@
 PROGRAM MAIN
     IMPLICIT NONE
     INTEGER, PARAMETER :: LL = SELECTED_INT_KIND(38)
-
-    INTEGER(LL) :: N, M, K, I, J, TMP, CNT, RET
-    INTEGER(LL), ALLOCATABLE :: X(:), Y(:)
-    INTEGER(LL) :: MODV = 9223372036737335297_LL, G = 3_LL
-
-    READ *, N, M
-
+    INTEGER(LL) :: MODV = 998244353, G = 3_LL
+    INTEGER(LL) :: N, M, L, K, I, J, TMP
+    INTEGER(LL), ALLOCATABLE :: X(:), Y(:), RET(:)
+    CHARACTER(LEN=4000010) :: XC, YC, BUF
+    READ (*, *) XC, YC
+    N = LEN_TRIM(XC)
+    M = LEN_TRIM(YC)
     K = 1
     DO WHILE (K <= N+M)
         K = K * 2
     END DO
-
-    ALLOCATE(X(0:K-1), Y(0:K-1))
-
-    X = 0_LL
-    Y = 0_LL
-
-    READ *, (X(I), I=0, N)
-    READ *, (Y(I), I=0, M)
-
+    ALLOCATE(X(0:K-1))
+    X = 0
+    DO I = 0, N - 1
+        X(I) = ICHAR(XC(N-I:N-I)) - ICHAR('0')
+    END DO
+    ALLOCATE(Y(0:K-1))
+    Y = 0
+    DO I = 0, M - 1
+        Y(I) = ICHAR(YC(M-I:M-I)) - ICHAR('0')
+    END DO
     CALL FFT(X, .FALSE.)
     CALL FFT(Y, .FALSE.)
     X = MODULO(X * Y, MODV)
     CALL FFT(X, .TRUE.)
-
-    RET = 0_LL
-    DO I=0, N+M
-        RET = IEOR(RET, X(I))
+    DO I = 0, K - 2
+        X(I+1) = X(I+1) + X(I) / 10
+        X(I) = MODULO(X(I), 10)
     END DO
-
-    PRINT "(I0)", RET
+    J = K - 1
+    DO WHILE (0<J .AND. X(J)==0)
+        J = J - 1
+    END DO
+    TMP = 1
+    DO I = J, 0, -1
+        BUF(TMP:TMP) = CHAR(X(I)+ICHAR('0'))
+        TMP = TMP + 1
+    END DO
+    WRITE(*, '(A)') BUF(1:TMP-1)
 
 CONTAINS
+
     SUBROUTINE FFT(V, INV)
         IMPLICIT NONE
         INTEGER, PARAMETER :: LL = SELECTED_INT_KIND(38)
-
         INTEGER(LL), INTENT(INOUT) :: V(0:)
         LOGICAL, INTENT(IN) :: INV
         INTEGER(LL) :: X, Y, WL, W
-        INTEGER(LL) :: MODV = 9223372036737335297_LL, G = 3_LL
-        INTEGER(LL) :: I, J, K, N, K2, STEP
-
+        INTEGER(LL) :: MODV = 998244353, G = 3_LL
+        INTEGER(LL) :: I, J, K, N, STEP
         N = SIZE(V)
-
         J = 0_LL
         DO I = 1, N-1
             K = SHIFTR(N, 1)
@@ -60,33 +66,29 @@ CONTAINS
                 V(J) = X
             END IF
         END DO
-
         K = 2_LL
         DO WHILE (K <= N)
             WL = POW(G, (MODV-1_LL)/K)
             IF (INV) WL = POW(WL, MODV-2_LL)
-            K2 = SHIFTR(K, 1)
             STEP = N / K
             DO I = 0, N-1, K
                 W = 1_LL
-                DO J = 0, K2-1
+                DO J = 0, SHIFTR(K, 1)-1
                     X = MODULO(V(I + J), MODV)
-                    Y = MODULO(V(I + J + K2) * W, MODV)
+                    Y = MODULO(V(I + J + SHIFTR(K, 1)) * W, MODV)
                     V(I + J)      = MODULO(X + Y, MODV)
-                    V(I + J + K2) = MODULO(X - Y, MODV)
+                    V(I + J + SHIFTR(K, 1)) = MODULO(X - Y, MODV)
                     W = MODULO(W * WL, MODV)
                 END DO
             END DO
             K = SHIFTL(K, 1_LL)
         END DO
-
         IF (INV) THEN
             W = POW(N, MODV-2_LL)
             DO I=0, N-1
                 V(I) = MODULO(V(I)*W, MODV)
             END DO
         END IF
-
     END SUBROUTINE FFT
 
     FUNCTION POW(BASE, EXP) RESULT(RET)
@@ -94,7 +96,7 @@ CONTAINS
         INTEGER, PARAMETER :: LL = SELECTED_INT_KIND(38)
         INTEGER(LL), INTENT(IN) :: BASE, EXP
         INTEGER(LL) :: RET, B, E
-        INTEGER(LL) :: MODV = 9223372036737335297_LL, G = 3_LL
+        INTEGER(LL) :: MODV = 998244353, G = 3_LL
         RET = 1_LL
         B = MODULO(BASE, MODV)
         E = EXP
